104d64b9fcda5c2a08f57ff2152da3aaa18889ac
Daniel Vu <dvv297@gmail.com>
2024-09-23 18:51:00 -0500

    Initial commit

---
+++ apps/api/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+
+const indexRouter = require("./routes/index");
+
+const port = 3000;
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/", indexRouter);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
