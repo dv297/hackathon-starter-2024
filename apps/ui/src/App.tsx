ed19fd1c3ee57a9a393106777d1fa2fd4159c6cc
Daniel Vu <dvv297@gmail.com>
2024-09-24 17:08:27 -0500

    Add ShadCN

--- apps/ui/src/App.tsx
+++ apps/ui/src/App.tsx
@@ -1,7 +1,18 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
-    <div className="flex justify-center items-center h-screen w-screen">
+    <div className="flex flex-col justify-center items-center h-screen w-screen">
       <h1 className="font-bold">Hackathon Starter Project</h1>
+      <div className="p-4">
+        <Button
+          onClick={() => {
+            alert("Success");
+          }}
+        >
+          Learn More
+        </Button>
+      </div>
     </div>
   );
 }
